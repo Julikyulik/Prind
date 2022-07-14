714a71712e993d775eefe51f0228fe9c3631db4b
Julikyulik <108988917+Julikyulik@users.noreply.github.com>
2022-07-14 21:27:49 +0300

    Update RepositoryPrinad.java

--- src/main/java/com/example/Prind/Repository/RepositoryPrinad.java
+++ src/main/java/com/example/Prind/Repository/RepositoryPrinad.java
@@ -24,29 +24,65 @@ public class RepositoryPrinad {
         ObjectMapper om = new ObjectMapper(new YAMLFactory());
         om.writeValue(new File("src/main/resources/prinad.yaml"), prinads);
     }
-    public void update(Prinad prinad, int snum) throws IOException {
-        List<Prinad> prinads = findALL();
+
+
+    public int update(Prinad prinad, int snum) throws IOException {
+        int status = 1;//статус по умолч. пишущая принадлежность отсутствует
+        boolean isFound = false;
+        boolean alreadyExist = false;
+        List<Prinad> prinads = findALL();//счит данных из файла
+
+        for (int i = 0; i < prinads.size(); i++){
+            if (prinads.get(i).getSerialNum() == prinad.getSerialNum()){
+              alreadyExist = true; //обновить сер. номер не возможно, т.к имеется пиш.принадлежность с таким сер.номером
+            }
+        }
         for (int i = 0; i < prinads.size(); i++){
             if (prinads.get(i).getSerialNum() == snum){
                 prinads.set(i, prinad);
+                isFound = true; // найдена пишущая принадлежность с сер.номером
             }
         }
-        addPrinad(prinads);
+        if (isFound != true){
+            status = 1;//не найден
+        }
+        else {
+            if (alreadyExist){
+                status = 2;// не можем заменить сер.номер, т.к такой сер.номер сущ. в пиш.прин;
+            }
+            else {
+                status = 0;//можем заменить сер.номер и ост.параметры
+                addPrinad(prinads);
+            }
+        }
+        return status;
     }
-    public Prinad getBySerialNumber(int snum) throws IOException, IllegalAccessError {
+    public Prinad getBySerialNumber(int snum) throws IOException {
         final List<Prinad> prinads = findALL();
         for (int i = 0; i < prinads.size(); i++){
             if (prinads.get(i).getSerialNum() == snum){
                 return prinads.get(i);
             }
         }
-        throw new IllegalAccessError("Not found choose another number!");
+        return null;
     }
-    public void deleteByPrinad(int snum) throws IOException {
+    public boolean deleteByPrinad(int snum) throws IOException {
+        final List<Prinad> prinadsBefore= findALL();
         final List<Prinad> prinads = findALL().stream()
                 .filter(c -> c.getSerialNum() != snum)
                 .collect(Collectors.toList());
-        addPrinad(prinads);
+        if (prinadsBefore != null && prinads != null){
+            if (prinadsBefore.size() > prinads.size()){
+                addPrinad(prinads);
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        else {
+            return false;
+        }
     }
     public void savePrinad(Prinad prinad) throws IOException {
         List<Prinad> prinads = findALL();
